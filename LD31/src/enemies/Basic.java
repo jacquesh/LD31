7243f6193ad53874cd861554817e54042751c2cd
Jacques Heunis <jaaksemail@gmail.com>
2014-12-07 10:59:04 +0200

    Make enemies get their textures from the background, complete with highlight around the edge

--- LD31/src/enemies/Basic.java
+++ LD31/src/enemies/Basic.java
@@ -1,5 +1,6 @@
 package enemies;
 
+import core.Main;
 import states.eControl;
 import util.Sprite;
 import util.Utility;
@@ -8,15 +9,20 @@ public class Basic extends Enemy
 {
     protected boolean splitter = false;
     protected int timer;
-    
+
     public Basic(double x, double y)
     {
-        super(x, y);
-        
+        this(x, y, (int)x, (int)y);
+    }
+
+    public Basic(double x, double y, int spawnX, int spawnY)
+    {
+        super(x, y, spawnX, spawnY);
+
         speedMax = 5;
         health = 4;
         rotationSpeed = (Math.random()-0.5)/2;
-        spr = new Sprite(Utility.loadImage("/Resources/Images/Enemies/enemyBase32.png"), 15.5, 15.5);
+        spr = new Sprite(Main.background.getSubimage(spawnX-16+2,spawnY-16+2, 28,28), 14, 14);
         
         if(Math.random()<0.15)
         {
@@ -42,7 +48,7 @@ public class Basic extends Enemy
                 {
                     for(int i=0; i<health; i++)
                     {
-                        Enemy e = new Splitlett(x, y);
+                        Enemy e = new Splitlett(x, y, spawnX, spawnY);
                         eControl.INSTANCE.attach(e);
                         e.direction = (i*0.5*Math.PI + rotation)%(2*Math.PI);
                     }
--- LD31/src/enemies/Enemy.java
+++ LD31/src/enemies/Enemy.java
@@ -3,17 +3,37 @@ package enemies;
 import java.awt.Graphics2D;
 import states.iObserver;
 import util.Sprite;
+import util.Utility;
 
 public abstract class Enemy implements iObserver
 {
+    private static Sprite highlight32;
+    private static Sprite highlight16;
+
     protected double x, y, direction, speed, speedMax, health;
     protected double rotation, rotationSpeed, fade;
     protected Sprite spr;
+
+    protected int spawnX;
+    protected int spawnY;
     
+    static
+    {
+        highlight32 = new Sprite(Utility.loadImage("/Resources/Images/Enemies/enemyBorder32.png"), 16, 16);
+        highlight16 = new Sprite(Utility.loadImage("/Resources/Images/Enemies/enemyBorder16.png"), 8, 8);
+    }
+
     public Enemy(double x, double y)
+    {
+        this(x, y, (int)x, (int)y);
+    }
+
+    public Enemy(double x, double y, int spawnX, int spawnY)
     {
         this.x = x;
         this.y = y;
+        this.spawnX = spawnX;
+        this.spawnY = spawnY;
         direction = 0;
         speed = 0;
         fade = 60;
@@ -56,6 +76,15 @@ public abstract class Enemy implements iObserver
     @Override
     public void draw(Graphics2D g)
     {
+        int w = spr.image.getWidth();
+        if(w == 28)
+        {
+            highlight32.draw(g,x,y,rotation);
+        }
+        else if(w == 12)
+        {
+            highlight16.draw(g,x,y,rotation);
+        }
         spr.draw(g, x, y, rotation);
     }
 }
